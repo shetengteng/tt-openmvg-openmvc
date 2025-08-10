afbba1502c0be5210ea7f597a813f5ee5bf928ca
shetengteng <work_stt@163.com>
2025-08-10 21:10:24 +0800

    updated

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,7 @@
 # 构建所有 Docker 镜像的脚本
 set -e
 
-echo "🚀 开始构建 OpenMVG, COLMAP, 和 OpenMVS 镜像..."
+echo "🚀 开始构建 OpenMVG, 和 OpenMVS 镜像..."
 
 # 构建 OpenMVG 镜像
 echo "📦 构建 OpenMVG v2.1 镜像..."
@@ -12,7 +12,3 @@ docker build --platform linux/amd64 -f Dockerfile.openmvg -t openmvg:v2.1 .
 # 构建 OpenMVS 镜像
 echo "📦 构建 OpenMVS v2.3.0 镜像..."
 docker build  --platform linux/amd64 -f Dockerfile.openmvs -t openmvs:v2.3.0 .
-
-# 构建 COLMAP 镜像
-echo "📦 构建 COLMAP 镜像..."
-docker build --platform linux/amd64  -f Dockerfile.colmap -t colmap:latest .
